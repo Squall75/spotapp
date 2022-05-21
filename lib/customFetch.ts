67d90b770dfb9c0ec174a7d1f20163bc346bf3ca
Todd Seabrook <toddseabrook@todds-mbp-3.lan>
2022-05-21 16:47:14 -0500

    init

---
+++ lib/customFetch.ts
@@ -0,0 +1,50 @@
+/**
+ * Custom Fetch is a fetcher that retries requests with exponential delay
+ *
+ * This custom fetch has the same interface as native fetch
+ * with three extra options for init param:
+ *  times    - number of attemps    (defaults 3)
+ *  delay    - basic delay in ms    (defaults 1000)
+ *  backoff  - exponential backoff  (defaults 1.3)
+ */
+
+ function sleep(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+  
+  export async function retryFetch(
+    input: RequestInfo,
+    init?: RequestInit & { times?: number; delay?: number; backoff?: number }
+  ) {
+    const times = init?.times || 3;
+    const delay = init?.delay || 1000;
+    const backoff = init?.backoff || 1.3;
+  
+    for (let attemp = 1; attemp <= times; attemp++) {
+      try {
+        const response = await fetch(input, init);
+  
+        if (response.status < 400) {
+          return response;
+        }
+  
+        throw new Error('Failed fetch');
+      } catch (error) {
+        console.warn('Fetch attemp failed', attemp);
+  
+        if (attemp < times) {
+          await sleep(delay * attemp * backoff);
+          continue;
+        }
+  
+        if (attemp === times) {
+          console.warn('Max attemps reached');
+  
+          throw error;
+        }
+      }
+    }
+  }
+  
+  export default retryFetch;
+  
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import { Box, Text} from '@chakra-ui/layout'
+import { Button } from '@chakra-ui/react'
+
+const Home = () => {
+
+  return (
+    <Box>
+      <Text>Authenticate with Spotify </Text>
+      <Button colorScheme='whatsapp' size='lg'>Spotify Auth</Button>
+  </Box>
+  )
+}
+
+export default Home
