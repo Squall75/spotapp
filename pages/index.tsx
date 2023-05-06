b713469d76a6fed00264a10e58771c2d6fedc10e
Squall75 <toddseabrook@todds-mbp.lan>
2023-05-06 14:50:57 -0500

    Clean up pages/index.tsx of eslint & prettier errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,31 @@
-import { Box, Text } from '@chakra-ui/layout';
+import { Box } from '@chakra-ui/layout';
+import { useState } from 'react';
 import SpotifyWebApi from '../lib/SpotifyApi';
 import oAuthManager from '../lib/oauthManager';
-import { useRef, useState } from 'react';
 import SpotifyAuthForm from '../components/SpotifyAuthForm';
 import ViewController from '../components/viewController';
 
 const Home = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [user, setUser] = useState();
-  const [api, setApi] = useState();
+  const [api, setApi] = useState<SpotifyWebApi>(undefined);
 
   const handleLoginClick = async () => {
-    const code = await oAuthManager.authorizationCode({
-      scopes: [
-        'playlist-read-private',
-        'playlist-read-collaborative',
-        'playlist-modify-public',
-        'playlist-modify-private',
-        'user-library-read',
-        'user-library-modify',
-        'user-follow-read',
-        'streaming',
-      ],
-    }).catch(function (error) {
-      console.error('There was an error obtaining the token', error);
-    });
-
-    if (global['ga']) {
-      global['ga']('send', 'event', 'spotify-dedup', 'user-logged-in');
-    }
-    console.log("Obtain token call");
+    const code = await oAuthManager
+      .authorizationCode({
+        scopes: [
+          'playlist-read-private',
+          'playlist-read-collaborative',
+          'playlist-modify-public',
+          'playlist-modify-private',
+          'user-library-read',
+          'user-library-modify',
+          'user-follow-read',
+          'streaming',
+        ],
+      })
+      .catch(function catchError(error) {
+        console.error('There was an error obtaining the token', error);
+      });
 
     const authToken = await oAuthManager.obtainToken(code);
 
@@ -38,8 +34,6 @@ const Home = () => {
     setApi(spotifyApi);
 
     try {
-      const signedInUser = await spotifyApi.getMe();
-      setUser(signedInUser);
       setIsLoggedIn(true);
     } catch (e) {
       console.error(e);
